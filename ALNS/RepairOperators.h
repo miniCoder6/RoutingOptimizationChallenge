0a4e194a14c9126e0c44d93808d22453aa8b6bbd
Purushottam Jha <j.purushottam@iitg.ac.in>
2026-02-06 18:47:39 +0530

    Update RepairOperators.h

--- ALNS/RepairOperators.h
+++ ALNS/RepairOperators.h
@@ -4,15 +4,20 @@
 #include "Vehicle.h"
 #include <vector>
 
+#include "CSVReader.h"
+
 void greedyRepair(std::vector<Route>&,
                   const std::vector<Employee>&,
-                  const std::vector<Vehicle>&);
+                  const std::vector<Vehicle>&,
+                  const Metadata&);
 
 void randomRepair(std::vector<Route>&,
                   const std::vector<Employee>&,
-                  const std::vector<Vehicle>&);
+                  const std::vector<Vehicle>&,
+                  const Metadata&);
 
 void regretRepair(std::vector<Route>&,
                   const std::vector<Employee>&,
                   const std::vector<Vehicle>&,
+                  const Metadata&,
                   int k = 2);
